9ec126179689b017bf257da7baff90f6fbd3c366
LLL2006 <zeikezan12345@gmail.com>
2024-11-25 23:35:32 +0700

    Update C SS5.8.c

--- C SS5.8.c
+++ C SS5.8.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 
-int ucln(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-int bcnn(int a, int b) {
-    return (a * b) / ucln(a, b);
-}
-
 int main() {
-    int n1, n2;
+    int n1, n2, bcnn;
     printf("Nhap hai so nguyen duong: ");
     scanf("%d %d", &n1, &n2);
 
-    if (n1 <= 0 || n2 <= 0) {
-        printf("Vui long nhap 2 so nguyen duong!\n");
-        return 1;
-    }
+     for(i=1;; i++) {
+        if (i % a == 0 && i % b == 0) {
+            bcnn = i;
+            break;
+        }
 
     printf("BCNN cua %d va %d la: %d\n", n1, n2, bcnn(n1, n2));
     return 0;
